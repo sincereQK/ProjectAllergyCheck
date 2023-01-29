3c948ce4f02de2d38c627822a3c7a3103f1841f0
sincereQK <mattmingyu@gmail.com>
2023-01-29 20:47:16 +0900

    change templates link

--- projectallergycheck/record/views.py
+++ projectallergycheck/record/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # record 부분 컨트롤
 def record_all_scan_list(request):
-    return render(request,'templates/record/all-scan-list.html')
+    return render(request,'./record/all-scan-list.html')
 
 def record_latest_scan_list(request):
-    return render(request,'templates/record/latest-scan-list.html')
+    return render(request,'./record/latest-scan-list.html')
--- projectallergycheck/scan/views.py
+++ projectallergycheck/scan/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 # scan 부분 컨트롤
 def scan_scan_list(request):
-    return render(request,'templates/scan/scan-list.html')
+    return render(request,'./scan/scan-list.html')
